674c56127026780e6906f0b8b88760a369580f5c
bruyns <brandon.bruyns7@gmail.com>
2023-04-05 11:02:59 +0200

    restructered js code and correct let and const variables

--- IWA5/IWA_5.2_Challenge-1/script.js
+++ IWA5/IWA_5.2_Challenge-1/script.js
@@ -1,53 +1,43 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = 0
-let location = 'RSA' || 'NAM'
-var shipping = 'shipping'
-var currency = 'R' || 'N$'
-customers = 1
-shiping = null
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = 0
+let location = 'RSA'
+let shipping = null
+let currency = null
+let customers = 1
 
+const shoes = 300 * 1
+const toys = 100 * 5
+const shirt = 150 * NONE_SELECTED
+const batteries = 35 * 2
+const pen = 5 * NONE_SELECTED
 
+const cost = (shoes + toys + shirt + batteries + pen)
+console.log(cost)
 
-if (location === 'RSA') 
-{ let shipping = 400 && currency === 'R' }
-
-if (location === 'NAM'){
-     let shipping = 600 && currency === 'N$'
+if (location === 'RSA'){
+     shipping = 400 
+} else if (location === 'NAM' ){
+   shipping = 600
+} else if (location !== 'RSA' || 'NAM'){
+     shipping = 800
+} else if (cost > 1000 && customers === 1){
+    shipping = 0
+}else{
+    console.log(BANNED_WARNING)
 }
-if (location !== 'NK'){
-    let shipping = 800
-}
 
+total = cost + shipping
+const notFree = 'RSA' + ' ' +  'R' + total;
+const freeShipping = 'RSA' +  ' ' + 'R' + cost ;
+const notRSA = 'NAM' + ' ' + 'N$' + total;
+const notNAM = '$' + total
 
-shoes = 300 * 1
-toys = 100 * 5
-shirts = 150 * NONE_SELECTED
-batteries = 35 * 2
-pens = 5 * NONE_SELECTED 
-
-var total = shoes + toys + shirts + batteries + pens
-
-
-	if (location === 'NAM' ||  location === 'RSA'  && customers < 2) {
-		    shipping = 0 && calcShipping
-		}
-
-
-if (shipping = 0 && customers !== 1) {
-    let shipping = 0
- console.log(FREE_WARNING) 
-}
-const price = shipping + total
-
-if (location !== 'RSA' || 'NAM'){ 
-   console.log(BANNED_WARNING) 
-}
-else{
-    console.log(total, currency, shoes + batteries + pens + shirts + shipping)
-}
-    
-console.log(currency, price, customers, shipping, location)
-customers = '1'
-
-currency = null
+console.log(notFree)
+// if ( notFree === true){
+// console.log(notFree)
+// }if (customers >1){
+//     console.log(FREE_WARNING)
+// }if (notRSA === true){
+//     console.log(notRSA)
+// }
