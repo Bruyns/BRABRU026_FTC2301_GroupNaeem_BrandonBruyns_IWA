8b15f9e63905018d8a0f3ee293cf12c0f493710a
Bruyns <brandon.bruyns7@gmail.com>
2023-04-06 14:15:19 +0200

    tested code during 1on1

--- IWA6/IWA_6.3_Challenge 1/script.js
+++ IWA6/IWA_6.3_Challenge 1/script.js
@@ -4,7 +4,7 @@ const secondaryPhone = '0219131568'
 // Only change below this line
 
 const primaryValid = typeof primaryPhone ===  "number"   //the term following typeof should be the variable being checked
-const secondaryValid = typeof secondaryPhone === "number" 
+const secondaryValid = typeof parseInt(secondaryPhone) === "number"
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
